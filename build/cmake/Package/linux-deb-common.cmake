1f8b4d57eba9567b10b02d3b533430a54e14b8b5
Oleg Kovalenko <monstrenyatko@gmail.com>
2018-01-14 20:37:15 -0600

    Update `deb` package dependencies.
    libssl1.0.0 (>= 1.0.1)
    libjansson4 (>= 2.3.1)

--- build/cmake/Package/linux-deb-common.cmake
+++ build/cmake/Package/linux-deb-common.cmake
@@ -4,5 +4,5 @@ set(CPACK_DEBIAN_PACKAGE_SECTION "net")
 set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE "i386")
 set(CPACK_SYSTEM_NAME "linux-${CPACK_DEBIAN_PACKAGE_ARCHITECTURE}")
 set(CPACK_DEBIAN_PACKAGE_DEPENDS
-	"libc6 (>= 2.13-28), libgcc1 (>= 1:4.8.2-21~), libstdc++6 (>=4.8.2-21~), lsb-base (>= 3.2-13)"
+	"libc6 (>= 2.13-28), libgcc1 (>= 1:4.8.2-21~), libstdc++6 (>=4.8.2-21~), lsb-base (>= 3.2-13), libssl1.0.0 (>= 1.0.1), libjansson4 (>= 2.3.1)"
 )
